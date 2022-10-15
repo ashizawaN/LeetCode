291a447e920021927b3cb9a8c6c78df69dbbdd6a
Natsuki Ashizawa <ashi2727n@gmail.com>
2022-10-15 15:18:38 +0900

    Update #888_Fair_Candy_Swap.py

--- #888_Fair_Candy_Swap.py
+++ #888_Fair_Candy_Swap.py
@@ -1,7 +1,8 @@
+from typing import List
 class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
-        x = (sum(aliceSizes) - sum(bobSizes)) // 2
-        
+        x = (sum(aliceSizes)-sum(bobSizes)) // 2
+
         for i in aliceSizes:
-            if i- x in set(bobSizes):
-                return [i,i-x]      
+            if i - x in bobSizes:
+                return [i, i-x]      
