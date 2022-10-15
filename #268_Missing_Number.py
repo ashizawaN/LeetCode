d0d754b4979ed4fc593d721691faa83348f6cc65
Natsuki Ashizawa <ashi2727n@gmail.com>
2022-10-15 15:26:36 +0900

    Update #268_Missing_Number.py

--- #268_Missing_Number.py
+++ #268_Missing_Number.py
@@ -1,7 +1,9 @@
+from typing import List
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        
+        num = set(nums)
+
         for i in range(n+1):
-            if i not in set(nums):
+            if i not in num:
                 return i
