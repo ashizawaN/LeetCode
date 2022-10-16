52f91c7a76fa5ea2f236bcc34862c7621dedad08
Natsuki Ashizawa <ashi2727n@gmail.com>
2022-10-16 11:03:47 +0900

    Update #852_Peak_Index_in_a_Mountain_Array.py

--- #852_Peak_Index_in_a_Mountain_Array.py
+++ #852_Peak_Index_in_a_Mountain_Array.py
@@ -1,17 +1,19 @@
 from typing import List
+
+
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         left = 0
         right = len(arr)
 
         while left <= right:
-            m = (left + right) // 2
+            mid = (left+right) // 2
             
-            if arr[m] < arr[m + 1]:
-                left = m + 1
-            elif arr[m] < arr[m - 1]:
-                right = m - 1
+            if arr[mid] < arr[mid + 1]:
+                left = mid + 1
+            elif arr[mid] < arr[mid - 1]:
+                right = mid - 1
             else:
-                return m
+                return mid
             
         
