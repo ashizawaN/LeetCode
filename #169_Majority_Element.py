5ce768bc6c784cd062a339ac6c87b8b1aaa94655
Natsuki Ashizawa <ashi2727n@gmail.com>
2022-10-21 13:09:51 +0900

    Update #169_Majority_Element.py

--- #169_Majority_Element.py
+++ #169_Majority_Element.py
@@ -3,5 +3,14 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+        count = 0
+        majority = 0
+        
+        for num in nums:
+            if count == 0:
+                majority = num
+            if majority == num:
+                count += 1
+            else:
+                count -= 1
+        return majority
