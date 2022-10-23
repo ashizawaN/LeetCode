7e5449a45cc03b0a89c485b284992afdef3949a3
Natsuki Ashizawa <113872082+ashizawaN@users.noreply.github.com>
2022-10-23 13:38:08 +0900

    Create #2418-2_Sort_the_People.py

---
+++ #2418-2_Sort_the_People.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        x = zip(heights, names)
+        sort_x = sorted(x, reverse=True)
+        heights, names = zip(*sort_x)
+        return names
