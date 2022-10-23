2b567a89d46f8a8b2ce615ea2fd0356d6aee621f
Natsuki Ashizawa <113872082+ashizawaN@users.noreply.github.com>
2022-10-23 16:08:45 +0900

    Create #2160-1_Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits.py

---
+++ #2160-1_Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumSum(self, num: int) -> int:
+        nums = [int(i) for i in list(str(num))]
+        
+        for i in range(1,len(nums)):
+            x = nums[i]
+            j = i - 1
+            
+            while j>=0 and nums[j]<x:
+                nums[j+1] = nums[j]
+                j -= 1
+            nums[j+1] = x
+
+        return nums[0] + nums[1] + nums[2]*10 + nums[3]*10
