a6f58963422153e700664fcc9d4c2ac5c5ca3cd1
Natsuki Ashizawa <ashi2727n@gmail.com>
2022-10-24 09:27:43 +0900

    Update #2160-3_Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits.py

--- #2160-3_Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits.py
+++ #2160-3_Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits.py
@@ -1,8 +1,11 @@
+from typing import List
+
+
 class Solution:
     def minimumSum(self, num: int) -> int:
         nums = [int(i) for i in list(str(num))]
 
-        def merge_sort(arr):
+        def merge_sort(arr: List[int]) -> int:
             if len(arr) <= 1:
                 return arr
             mid = len(arr) // 2
@@ -12,8 +15,7 @@ class Solution:
             right = merge_sort(right)
             return merge(left, right)
 
-
-        def merge(left, right):
+        def merge(left: int, right: int) -> int:
             merged = []
             l_i, r_i = 0, 0
             while l_i < len(left) and r_i < len(right):
@@ -23,13 +25,11 @@ class Solution:
                 else:
                     merged.append(right[r_i])
                     r_i += 1
-
             if l_i < len(left):
                 merged.extend(left[l_i:])
             if r_i < len(right):
                 merged.extend(right[r_i:])
             return merged
         
-        sort_nums = merge_sort(nums)
-            
+        sort_nums = merge_sort(nums)    
         return sort_nums[0]*10 + sort_nums[1]*10 + sort_nums[2] + sort_nums[3]
