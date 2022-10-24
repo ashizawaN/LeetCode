e5d495b563bec3821c692e15950f905346761fd2
Natsuki Ashizawa <ashi2727n@gmail.com>
2022-10-24 09:17:23 +0900

    Update #2418-1_Sort_the_People.py

--- #2418-1_Sort_the_People.py
+++ #2418-1_Sort_the_People.py
@@ -4,14 +4,14 @@ from typing import List
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         for i in range(1, len(heights)):
-            x_names = names[i]
-            x_heights = heights[i]
+            x_name = names[i]
+            x_height = heights[i]
             j = i - 1
             
-            while j>=0 and heights[j]<x_heights:
+            while j >= 0 and heights[j] < x_height:
                 names[j+1], heights[j+1] = names[j], heights[j]
                 j -= 1
-            names[j+1] = x_names
-            heights[j+1] = x_heights
+            names[j+1] = x_name
+            heights[j+1] = x_height
             
         return names
