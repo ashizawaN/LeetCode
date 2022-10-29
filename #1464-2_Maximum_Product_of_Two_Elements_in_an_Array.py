eb94aeb79e3c718006ea0ad1fb6feb1763f92ec3
Natsuki Ashizawa <natsuki@natsuki.local>
2022-10-29 20:37:19 +0900

    #1464-2_Maximum_Product_of_Two_Elements_in_an_Array.py

---
+++ #1464-2_Maximum_Product_of_Two_Elements_in_an_Array.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
+        MIN_NUM = 1
+        MAX_NUM = 10**3
+        MIN_LENGTH = 2
+        MAX_LENGTH = 500
+        assert type(nums) == list, "The type of nums should be list."
+        if len(nums) < MIN_LENGTH or len(nums) > MAX_LENGTH:
+            raise ValueError("The range of nums should be 2 or more and 500 or less.")
+        for i in range (len(nums)-1):
+            if nums[i] < MIN_NUM or nums[i] > MAX_NUM:
+                raise ValueError("The value of nums shoud be 1 or more and 10**3 or less.")
+        
+        i = 0
+        j = 0
+        for number in nums:
+            if number > i:
+                i, j = number, i
+            elif number > j:
+                j = number
+        return (i-1) * (j-1)
