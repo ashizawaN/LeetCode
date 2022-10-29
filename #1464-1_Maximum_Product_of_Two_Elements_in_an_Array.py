a3d5188e190386d875f63aa07c23922537862287
Natsuki Ashizawa <natsuki@natsuki.local>
2022-10-29 20:35:56 +0900

    create #1464-1_Maximum_Product_of_Two_Elements_in_an_Array.py

---
+++ #1464-1_Maximum_Product_of_Two_Elements_in_an_Array.py
@@ -0,0 +1,28 @@
+from typing import List
+import heapq
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
+        MIN_NUM = 1
+        MAX_NUM = 10**3
+        MIN_LENGTH = 2
+        MAX_LENGTH = 500
+        assert type(nums) == list, "The type of nums should be list."
+        if len(nums) < MIN_LENGTH or len(nums) > MAX_LENGTH:
+            raise ValueError("The range of nums should be 2 or more and 500 or less.")
+        for i in range (len(nums)-1):
+            if nums[i] < MIN_NUM or nums[i] > MAX_NUM:
+                raise ValueError("The value of nums shoud be 1 or more and 10**3 or less.")
+        
+        heap = []
+        heapq.heapify(heap)
+        for num in nums:
+            if len(heap) <= 1:
+                heapq.heappush(heap, num)
+            else:
+                heapq.heappush(heap, num)
+                heapq.heappop(heap)
+        
+        return (heapq.heappop(heap)-1) * (heapq.heappop(heap)-1)
