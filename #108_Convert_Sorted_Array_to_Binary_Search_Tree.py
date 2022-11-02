a9ecf7a4fb7d10684d4c1e1c5c03935a49ff99eb
Natsuki Ashizawa <natsuki@natsuki.local>
2022-11-02 13:19:05 +0900

    create #108_Convert_Sorted_Array_to_Binary_Search_Tree.py

---
+++ #108_Convert_Sorted_Array_to_Binary_Search_Tree.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if len(nums) == 0:
+            return
+        low = 0
+        high = len(nums) - 1
+        mid = (high + low) // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        return root
