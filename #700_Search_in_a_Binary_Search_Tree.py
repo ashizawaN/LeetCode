46d91a68cc0e8b34b88d82d493f3dc4e569060e0
Natsuki Ashizawa <natsuki@natsuki.local>
2022-11-04 17:59:58 +0900

    create #700_Search_in_a_Binary_Search_Tree.py

---
+++ #700_Search_in_a_Binary_Search_Tree.py
@@ -0,0 +1,19 @@
+# time complexity: O(n)
+# space complexity: O(1)
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        if root.val < val:
+            return self.searchBST(root.right, val)
+        if root.val > val:
+            return self.searchBST(root.left, val)
