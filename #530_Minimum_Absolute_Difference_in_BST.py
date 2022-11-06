ca04cfa6238a8f649cbd7ac62d906fd4e7b20a6c
Natsuki Ashizawa <natsuki@natsuki.local>
2022-11-06 13:16:09 +0900

    update #530_Minimum_Absolute_Difference_in_BST.py

--- #530_Minimum_Absolute_Difference_in_BST.py
+++ #530_Minimum_Absolute_Difference_in_BST.py
@@ -1,10 +1,11 @@
 # time complexity: O(n)
-# space complexity: O(1)
+# space complexity: O(logn)
 from math import inf
+from typing import Optional
 
 
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self, val: int = 0, left: Optional["TreeNode"] = None, right: Optional["TreeNode"] = None) -> None:
         self.val = val
         self.left = left
         self.right = right
@@ -15,6 +16,7 @@ class Solution:
         def rec(node: TreeNode, low: int, high: int) -> int:
             if not node:
                 return high - low
+
             left = rec(node.left, low, node.val)
             right = rec(node.right, node.val, high)
             return min(left, right)
