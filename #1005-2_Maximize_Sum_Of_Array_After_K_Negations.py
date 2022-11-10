a54897916a6fb6eb165874c7d32578b186c7f616
Natsuki Ashizawa <natsuki@natsuki.local>
2022-11-10 12:31:14 +0900

    update #1005-2_Maximize_Sum_Of_Array_After_K_Negations.py

--- #1005-2_Maximize_Sum_Of_Array_After_K_Negations.py
+++ #1005-2_Maximize_Sum_Of_Array_After_K_Negations.py
@@ -1,18 +1,14 @@
 # time complexity: O(nlogn)
-# space complexity: O(1)
+# space complexity: O(n)
 import heapq
 from typing import List
 
 
 class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
-        heap = []
-        for i in range(len(nums)):
-            heapq.heappush(heap, nums[i])
-
+        heapq.heapify(nums)
         while k:
-            num = heapq.heappop(heap)
-            num = -num
-            heapq.heappush(heap, num)
+            num = heapq.heappop(nums)
+            heapq.heappush(nums, -num)
             k -= 1
-        return sum(heap)
+        return sum(nums)
