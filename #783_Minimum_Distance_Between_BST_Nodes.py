1c8372ce52d03b848feffad29c24d7903103510f
Natsuki Ashizawa <natsuki@natsuki.local>
2022-11-13 00:12:48 +0900

    update #783_Minimum_Distance_Between_BST_Nodes.py

--- #783_Minimum_Distance_Between_BST_Nodes.py
+++ #783_Minimum_Distance_Between_BST_Nodes.py
@@ -1,18 +1,19 @@
 # time complexity: O(n)
 # space complexity: O(n)
 from math import inf
+from typing import Optional
 
 
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None) -> None:
+    def __init__(self, val=0, left: Optional["TreeNode"] = None, right: Optional["TreeNode"] = None) -> None:
         self.val = val
         self.left = left
         self.right = right
 
 
 class Solution:
-    def minDiffInBST(self, root: TreeNode) -> int:
-        def rec(node: TreeNode, low: int, high: int) -> int:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        def rec(node: Optional[TreeNode], low: int, high: int) -> int:
             if not node:
                 return high - low
             left = rec(node.left, low, node.val)
