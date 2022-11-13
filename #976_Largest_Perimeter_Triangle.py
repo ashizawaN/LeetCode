1afda0a048da3661bfc4b5326529af38dc5fc5d7
Natsuki Ashizawa <natsuki@natsuki.local>
2022-11-13 10:21:19 +0900

    create #976_Largest_Perimeter_Triangle.py

---
+++ #976_Largest_Perimeter_Triangle.py
@@ -0,0 +1,12 @@
+# time complexity: O(nlogn)
+# space complexity: O(1)
+from typing import List
+
+
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        for i in range(3, len(nums)+1):
+            if nums[i-3] < nums[i-2] + nums[i-1]:
+                return sum(nums[i-3:i])
+        return 0
