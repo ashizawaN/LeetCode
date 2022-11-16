acaa4ce8a445283f42c74980a53888de8901ac23
Natsuki Ashizawa <113872082+ashizawaN@users.noreply.github.com>
2022-11-16 09:36:19 +0900

    Update #1005-1_Maximize_Sum_Of_Array_After_K_Negations.py
    I tried solving the problem. However, complexity didn't change by using bisect. So, I do not understand the intent of the rewrite. I would like to know if this code is correct and why it is necessary to use bisect.

--- #1005-1_Maximize_Sum_Of_Array_After_K_Negations.py
+++ #1005-1_Maximize_Sum_Of_Array_After_K_Negations.py
@@ -1,5 +1,6 @@
 # time complexity: O(n**2)
 # space complexity: O(1)
+import bisect
 from typing import List
 
 
@@ -7,6 +8,7 @@ class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
         nums.sort()
         for i in range(k):
-            nums[0] *= -1
-            nums.sort()
+            cal = nums[0]*-1
+            nums.pop(0)
+            bisect.insort_left(nums, cal)
         return sum(nums)
